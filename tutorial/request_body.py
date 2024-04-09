80f48cb026a4f6d50206954c32ec0668eb5f18ed
Heo GeonYeong <gyheo@icloud.com>
2024-04-09 17:29:53 +0900

    feat: Add request body

---
+++ tutorial/request_body.py
@@ -0,0 +1,49 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Import Pydantic's BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+# Use the model
+@app.post("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+# Request body + path parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+
+# Request body + path + query parameters
+@app.put("/items_query/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
